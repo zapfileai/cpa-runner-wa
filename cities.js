b7e28a3cb4af25d81df1606943886000c267b677
Sreehari Annam <sannam@wescodist.com>
2026-06-30 07:30:35 -0400

    feat: load first 80 cities from cities.csv

--- cities.js
+++ cities.js
@@ -1,5 +1,20 @@
 const washingtonCities = [
-  // paste first 80 cities from cities.csv here
+  "Seattle", "Spokane", "Bellevue", "Bellingham", "Lynnwood",
+  "Eastsound", "Ephrata", "Tacoma", "Richland", "Farmington",
+  "Greenacres", "Harrington", "Kent", "Carlton", "Albion",
+  "Fall City", "Bothell", "Bremerton", "Freeland", "Grandview",
+  "Camano Island", "Black Diamond", "Kenmore", "Fox Island", "Hobart",
+  "Carson", "Allyn", "Dupont", "Centralia", "Creston",
+  "Fairfield", "George", "Clarkston", "Beaver", "Arlington",
+  "Cinebar", "Forks", "Kelso", "Elma", "Cle Elum",
+  "East Wenatchee", "Connell", "Cashmere", "Garfield", "Cowiche",
+  "Aberdeen", "Hoodsport", "Airway Heights", "Coulee Dam", "Davenport",
+  "Cosmopolis", "Blaine", "Everett", "Kapowsin", "Granite Falls",
+  "Lake Stevens", "Federal Way", "Camas", "Liberty Lake", "Amboy",
+  "Dayton", "Brush Prairie", "Bainbridge Island", "Vancouver", "Issaquah",
+  "Indianola", "Grapeview", "Mercer Island", "Burbank", "Ione",
+  "Friday Harbor", "Fairchild", "Bingen", "Goldendale", "Colfax",
+  "Coupeville", "Redmond", "Kennewick", "Granger", "Gig Harbor",
 ];
 
 const BATCH_SIZE = 5;
